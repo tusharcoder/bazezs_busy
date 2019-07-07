5ab8aa69812962100ae2742ef727d7297f96a1b9
Tushar Agarwal(tusharcoder) <tushar@scaledesk.com>
2019-07-07 16:36:22 +0530

    remove info from constant.php

--- constant.php
+++ constant.php
@@ -2,12 +2,4 @@
 //reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
 define('SITE_KEY','6LezNqoUAAAAAFUh425ybN9tia4yajFO-okdTnzw'); 
 define('SECRET_KEY','6LezNqoUAAAAAIhR5D15a1DatSAKAFRfcwo2RmzS');
-define('HOST','smtp.gmail.com');
-define('USERNAME','tamyworld@gmail.com');
-define('FROM',USERNAME);
-define('FROMNAME','Admin');
-define('PASSWORD','cfnmktahvdvketuf');
-define('SMTPSECURE','ssl');
-define('PORT',465);
-define('SMTPAUTH',true);
 ?>
