f29004ee18155442d94befdad6cac353f2bd4250
Tushar Agarwal(tusharcoder) <tushar@scaledesk.com>
2019-07-16 21:35:33 +0530

    add the data to the alipay

--- do_alipay_payment.php
+++ do_alipay_payment.php
@@ -5,16 +5,16 @@ set up the post data from the form
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$name = $_POST["user_name"]; 
-$email = $_POST["user_email"]; 
-$address = $_POST["user-address"]; 
-$city = $_POST["user-city"]; 
-$postal = $_POST["user-postal"]; 
-$country = $_POST["user-country"];
-$price = $_POST['price'];
-$discount = $_POST['discount'];
+$name = trim($_POST["user_name"]); 
+$email = trim($_POST["user_email"]); 
+$address = trim($_POST["user-address"]); 
+$city = trim($_POST["user-city"]); 
+$postal = trim($_POST["user-postal"]); 
+$country = trim($_POST["user-country"]);
+$price = trim($_POST['price']);
+$discount = trim($_POST['discount']);
 $host = trim($_POST['host']);
-$order_id = $_POST['order_id'];
+$order_id = trim($_POST['order_id']);
 
 $alipay_amt = 100*((float)$price-(float)$discount);
 							$alipay_cur = "usd";
@@ -27,7 +27,13 @@ $alipay_amt = 100*((float)$price-(float)$discount);
 							  "currency" => $alipay_cur,
 							  "amount"=>$alipay_amt,
 							  "owner" => [
-							    "email" => "test@test.com"
+							    "address"=>[
+							      "line1" => $address,
+							      "city" => $city,
+							      "postal_code" => $postal,
+							      "country" => $country],
+    							"name"=> $name,
+    							"email" => $email
 							  ],
 							  "redirect"=>[
 							  	"return_url"=>$host."validate_payment.php?amount=".$alipay_amt."&currency=".$alipay_cur."&order_id=".$order_id
