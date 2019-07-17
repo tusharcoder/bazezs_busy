cc1dbf5aa89e573df4d8171c95466ee16e0c2f69
tusharcoder <tushar@8415>
2019-07-17 21:00:35 +0530

    add the radio switch

--- admin/admin_stripe.php
+++ admin/admin_stripe.php
@@ -19,6 +19,72 @@
 		$stripe_live_secret_key = $row['stripe_live_secret_key'];
 		$change_price = $row['change_price'];
 	?>
+
+    <style type="text/css">
+        /*switch style */
+        .switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+/*end switch style */
+    </style>
 </head>
 <body>
 <section id="container">
@@ -40,8 +106,19 @@
             <div class="panel-body">
                 <form action="operations.php" method="POST">
                     <div class="form-group row">
-						<div class="col-sm-1"></div>
+                        <div class="col-sm-1"></div>
                         <label class="col-sm-3 control-label"><b>Test Secret Key:</b></label>
+                        <div class="col-sm-6">
+                        <!-- Rounded switch -->
+<label class="switch">
+  <input type="checkbox">
+  <span class="slider round"></span>
+</label>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+						<div class="col-sm-1"></div>
+                        <label class="col-sm-3 control-label"><b>Live Payment Gateway:</b></label>
                         <div class="col-sm-6">
                             <input type="text" name="stripe_test_secret_key" class="form-control" value="<?php echo $stripe_test_secret_key?>" required>
                         </div>
